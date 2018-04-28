60b4a3645eacb758a8d01ba096d291cd93795bae
mickle.jiang <mickle.jiang@rcchina.com>
2018-04-28 18:06:42 +0800

    fix

--- src/main/index.js
+++ src/main/index.js
@@ -15,20 +15,38 @@ const winURL = process.env.NODE_ENV === 'development'
   ? `http://localhost:9080`
   : `file://${__dirname}/index.html`
 
+let loadingUrl = process.env.NODE_ENV !== 'development' ? `file://${__dirname}/static/loading.html` : 'http://127.0.0.1:9080/static/loading.html'
+
 function createWindow () {
+  let loginWin = null
+
+  loginWin = new BrowserWindow({width: 658, height: 493, frame: false, titleBarStyle: 'hiddenInset'})
+  loginWin.loadURL(loadingUrl)
+
   /**
    * Initial window options
    */
   mainWindow = new BrowserWindow({
     height: 563,
-    useContentSize: true,
-    width: 1000
+    useContentSize: false,
+    width: 1000,
+    show: false,
+    backgroundColor: '#FFF',
+    icon: process.env.NODE_ENV !== 'development' ? `file://${__dirname}/static/icon.ico` : 'http://127.0.0.1:9080/static/icon.ico'
   })
 
   mainWindow.loadURL(winURL)
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show()
+    if (loginWin) {
+      loginWin.hide()
+      loginWin = null
+    }
+  })
 
   mainWindow.on('closed', () => {
     mainWindow = null
+    app.exit()
   })
 }
 
